c934b700c83fb5fa7ea08501365e0d8784b9c041
RakeshMundel <rakeshmundel000@gmail.com>
2026-01-31 20:43:07 +0530

    Find Smallest Letter Greater Than Target

---
+++ POTD_744.java
@@ -0,0 +1,21 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+      int left = 0 ; 
+      int right = letters.length-1;
+      int ind=0;
+      while(left<=right){
+        int mid = (left+right)/2;
+        if(letters[mid]<=target){
+            left=mid+1;
+            
+            }
+            else {
+                right=mid-1;
+                ind=mid;
+            }
+
+    }
+
+             return letters[ind];
+    }
+}
