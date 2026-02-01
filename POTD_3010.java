1d566c6b4adc6fc39a52159b62729b3a7254a146
RakeshMundel <rakeshmundel000@gmail.com>
2026-02-01 13:41:59 +0530

    Divide an Array into Subarrays with minimum cost I

---
+++ POTD_3010.java
@@ -0,0 +1,17 @@
+public class POTD_3010 {
+    public int minimumCost(int[] nums) {
+        int first=Integer.MAX_VALUE;
+            int second=Integer.MAX_VALUE;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<first){
+                second=first;
+                first=nums[i];
+                
+            }else if(nums[i]<second){
+                second=nums[i];
+    
+            }
+        }
+        return nums[0]+first+second;
+    }
+}
